d345afc755434f2bc5ddddd72250fb13b08696a7
Michelle-Mandujano-UwU <160373359+Michelle-Mandujano-UwU@users.noreply.github.com>
2026-04-28 10:55:53 -0600

    suma

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Esta es una respuesta sencilla')
 })
+app.get('/Suma', (req, res) => {
+  const {a, b} = req.query;
+  const resultado = Number(a) + Number(b);  
+  res.json({ resultado });
 
+})
 module.exports = app;
